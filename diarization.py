6d21cf54cbaed02e2f75a83d4120d1b80ebe6e7c
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-09-20 00:25:56 +0200

    outlined diarization pipeline using whisperx

--- diarization.py
+++ diarization.py
@@ -1,10 +1,10 @@
 import whisperx
-import gc 
+import gc
 
-device = "cuda" 
-audio_file = "audio.mp3"
+device = "cpu"
+audio_file = "output5.mp3"
 batch_size = 16 # reduce if low on GPU mem
-compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy)
 
 # 1. Transcribe with original whisper (batched)
 model = whisperx.load_model("large-v2", device, compute_type=compute_type)
@@ -26,10 +26,10 @@ print(result["segments"]) # after alignment
 # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
 # 3. Assign speaker labels
-diarize_model = whisperx.DiarizationPipeline(use_auth_token=YOUR_HF_TOKEN, device=device)
+diarize_model = whisperx.DiarizationPipeline(use_auth_token="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", device=device)
 
 # add min/max number of speakers if known
-diarize_segments = diarize_model(audio)
+diarize_segments = diarize_model(audio, min_speakers=2, max_speakers=2)
 # diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
 
 result = whisperx.assign_word_speakers(diarize_segments, result)
